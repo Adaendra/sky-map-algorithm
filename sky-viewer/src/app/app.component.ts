020a22283d94922674b30b75f64174fd896cc129
redfire <maxime.guilbert@neuf.fr>
2020-05-16 11:42:49 -0400

    Ménage

--- sky-viewer/src/app/app.component.ts
+++ sky-viewer/src/app/app.component.ts
@@ -1,19 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {Main} from "../../../typescript/Main";
-import starsData from "./../../../data/stars_data.json";
-import constelationData from "./../../../data/constellations_data.json";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-
-  ngOnInit(): void {
-    console.log(starsData);
-    console.log(constelationData);
-    Main.hello();
-  }
+export class AppComponent {
 
 }
